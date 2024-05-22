638a2e933fd81861ca0094565b00b6cc20a39045
Crowdbotics <team@crowdbotics.com>
2024-05-22 15:20:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bookstore-48060",
+  url: "https://bookstore-48060.botics.co",
+  api: "https://bookstore-48060.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
